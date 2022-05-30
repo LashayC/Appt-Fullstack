5d9c05d669db15375345c09cd05a8b79a87775df
LashayC <cokleylashay@gmail.com>
2022-05-30 13:28:10 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://shay:SHCBk99XpMHfBkqX@cluster0.2n0wm.mongodb.net/work-it-out-log?retryWrites=true&w=majority',
+    'dbName': 'work-it-out-log'
+};
